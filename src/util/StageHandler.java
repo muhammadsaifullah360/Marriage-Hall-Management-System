6289bf85fee1dcb0f4f4cb6e40cc82c778b71c86
Saif Ali <54573967+saifali-ch@users.noreply.github.com>
2020-12-10 00:20:09 +0500

    Created utility class StageHandler.

---
+++ src/util/StageHandler.java
@@ -0,0 +1,32 @@
+package util;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StageHandler {
+    static public Stage createStage(String fxmlPath, String title) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(StageHandler.class.getResource("/forget/password/ForgetPassword.fxml"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("FXML Loading Error!!!");
+        }
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setTitle(title);
+        stage.setScene(scene);
+        stage.show();
+        return stage;
+    }
+    
+    public static Stage getStage(ActionEvent event) {
+        return (Stage) ((Node) event.getSource()).getScene().getWindow();
+    }
+}
