39cec10e2455abb08cf8a0756ab104c0dcbf2ccc
Saif Ali <54573967+saifali-ch@users.noreply.github.com>
2020-12-10 00:21:11 +0500

    Refactored LoginController

--- src/main/LoginController.java
+++ src/main/LoginController.java
@@ -1,73 +1,51 @@
 package main;
 
-import database.DBService;
-import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
-import javafx.event.ActionEvent;
+import database.DBService;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
+import util.StageHandler;
 
-import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class LoginController {
+    @FXML
+    private JFXTextField username_field;
+    @FXML
+    private JFXPasswordField password_field;
+    @FXML
+    private Label loginMessage_label;
     
     @FXML
-    private JFXButton forgetpasswordbtn;
-    
-    @FXML
-    private JFXTextField username_txt;
-    
-    @FXML
-    private JFXPasswordField password_txt;
-    
-    @FXML
-    private Label loginMessage;
-    
-    @FXML
-    public void loginButton_txt(ActionEvent event) throws IOException {
-        if (username_txt.getText().isBlank() && password_txt.getText().isBlank()) {
-            loginMessage.setText("Please enter the username and password!");
-        } else if (validateLogin()) {
-            Parent root = FXMLLoader.load(getClass().getResource("/dashboard/Dashboard.fxml"));
-            Scene scene = new Scene(root);
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(scene);
-            stage.setTitle("Dashboard Of Hall Management System");
-            stage.show();
-        }
+    private void login() {
+        String username = username_field.getText();
+        String password = password_field.getText();
+        if (username.isBlank() || password.isBlank())
+            loginMessage_label.setText("Please Enter Username and Password!");
+        else if (exists(username, password)) {
+            String fxmlPath = "/dashboard/Dashboard.fxml";
+            String title = "Dashboard Of Hall Management System";
+            StageHandler.createStage(fxmlPath, title).show();
+        } else loginMessage_label.setText("Username or Password Incorrect!");
     }
     
-    public boolean validateLogin() {
-        boolean ok = false;
+    private boolean exists(String username, String password) {
+        String query = String.format("Select * From SignUp Where username='%s' AND password='%s'", username, password);
+        ResultSet rs = DBService.executeQuery(query);
         try {
-            String query = String.format("select * from signup where username = '%s' AND password = '%s'", username_txt.getText(), password_txt.getText());
-            ResultSet rs = DBService.statement.executeQuery(query);
-            if (rs.next())
-                ok = true;
-            else
-                ok = false;
-            loginMessage.setText("Wrong Username and Password! ");
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+            return rs.next();
+        } catch (SQLException sqlException) {
+            System.out.println(sqlException.getMessage());
         }
-        return ok;
+        return false;
     }
     
-    
-    public void forgetpasswordbtn(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/forget/password/ForgetPassword.fxml"));
-        Scene scene = new Scene(root);
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setTitle("Forget Password");
-        stage.setScene(scene);
-        stage.show();
+    @FXML
+    private void forgetPassword() {
+        String fxmlPath = "/forget/password/ForgetPassword.fxml";
+        String title = "Forget Password";
+        StageHandler.createStage(fxmlPath, title).show();
     }
 }
